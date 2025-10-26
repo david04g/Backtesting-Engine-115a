4ca5c583e658fdc127ec82e626b76a2659d6b7fc
nsnhamucsc <nsnham@ucsc.edu>
2025-10-26 16:42:13 -0700

    Added user profile pictures

--- db_supabase/db_test.py
+++ db_supabase/db_test.py
@@ -1,10 +1,16 @@
 import db_util
 
-result = db_util.add_user("test_user", "test@example.com", "pass_hash_123")
-print(f"Result: {result}")
+#db_util.delete_user_by_id(51)
+#result = db_util.add_user("pfp_test2", "pfp_test2@gmail.com", "pass_hash_123")
+#print(f"Result: {result} \n")
 
-result = db_util.get_user_by_id(2)
-print(f"Result: {result}")
+#user_id = result["user"]["id"]
+#print(user_id)
 
-#db_util.delete_user_by_id(2)
-db_util.delete_user_by_username("test_user")
+#print(new_uid)
+#result = db_util.get_user_by_id(2)
+#print(f"Result: {result}")
+
+#db_util.delete_user_by_username("pfp_test2")
+db_util.upload_profile_picture_by_user_id(61, "/app/26db8e1bc9a0eb238e69ae7d01a24fee.jpg")
+#db_util.delete_user_by_id(user_id)
--- db_supabase/db_util.py
+++ db_supabase/db_util.py
@@ -1,6 +1,9 @@
 import os
 import dotenv
 from supabase import Client, create_client
+import bcrypt
+import imghdr
+import mimetypes
 
 dotenv.load_dotenv()
 
@@ -9,12 +12,15 @@ SUPABASE_PASS = os.getenv("SUPABASE_PASS")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_PASS)
 
-def add_user(username: str, email: str, password_hash: str):
+def add_user(username: str, email: str, password_plain: str):
     try:
+        password_hash = bcrypt.hashpw(password_plain.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         response = supabase.table("users").insert({
             "username": username,
             "email": email,
-            "password_hash": password_hash
+            "password_hash": password_hash,
+            #insert default pfp
+            "profile_image": "https://qlgyxqafprlghppeqjrk.supabase.co/storage/v1/object/public/profile-pictures/default_profile_image.png"
         }).execute()
         if response.data:
             return {"success": True, "user": response.data[0]}
@@ -25,7 +31,23 @@ def add_user(username: str, email: str, password_hash: str):
             return {"success": False, "message": "user already exists"}
         else:
             return {"success": False, "message": f"error adding user: {e}"}
-        
+
+def login_user(email: str, password_plain: str):
+    try:
+        response = supabase.table("users").select("*").eq("email", email).execute()
+        if not response.data or len(response.data) == 0:
+            return {"success": False, "message": "User email not found"}
+
+        user = response.data[0]
+        stored_hash = user["password_hash"]
+        if bcrypt.checkpw(password_plain.encode("utf-8"), stored_hash.encode("utf-8")):
+            return {"success": True, "user": user}
+        else:
+            return {"success": False, "message": "Invalid password"}
+
+    except Exception as e:
+        return {"success": False, "message": f"Error logging in: {e}"}
+
 def get_user_by_id(uid: int):
     response = supabase.table("users").select("*").eq("id", uid).execute()
     if response.data:
@@ -44,7 +66,6 @@ def get_all_users():
     response = supabase.table("users").select("*").execute()
     return response.data if response.data else []
 
-
 def delete_user_by_username(username: str):
     response = supabase.table("users").delete().eq("username", username).execute()
     return {"deleted": len(response.data)} if response.data else {"deleted": 0}
@@ -52,3 +73,47 @@ def delete_user_by_username(username: str):
 def delete_user_by_id(uid: int):
     response = supabase.table("users").delete().eq("id", uid).execute()
     return {"deleted": len(response.data)} if response.data else {"deleted": 0}
+
+def is_image_file(path: str):
+    file_type = imghdr.what(path)
+    if file_type in ["jpeg", "png"]:
+        return True
+    else:
+        return False
+
+def upload_profile_picture_by_user_id(uid: int, file_path: str):
+    bucket = "profile-pictures"
+    file_name = f"user_{uid}.png"
+    
+    if not os.path.exists(file_path):
+        print(f"File not found: {file_path}")
+        return None
+    
+    if not is_image_file(file_path):
+        print("Invalid file type. Only image files (jpg/png) are allowed.")
+        return None
+
+    mime_type, dummy = mimetypes.guess_type(file_path)
+    if mime_type is None:
+        mime_type = "image/png" #fallback if fails, not sure if this is the best idea
+
+    
+
+    with open(file_path, "rb") as f:
+        #if user_{uid}.png already exists, it will replace the user_{uid}.png photo
+        res = supabase.storage.from_(bucket).upload(file_name, f, {"upsert": "true", "content-type": mime_type})
+
+    """
+    if res:
+        print("Profile picture upload failed: ", res)
+        return None
+    """
+    #note: as of now, there is no error handling. will be added in future
+
+    #build public url
+    public_url = f"{os.getenv('SUPABASE_URL')}/storage/v1/object/public/{bucket}/{file_name}"
+
+    #updates the db
+    supabase.table("users").update({"profile_image": public_url}).eq("id", uid).execute()
+
+    return public_url
