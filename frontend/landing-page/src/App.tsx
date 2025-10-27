57b47cdc67fd52f64a7c8c1df42f5a17f05aa3ed
Rohan Shukla <rohan.shukla@gmail.com>
2025-10-27 16:08:30 -0700

    Consolidate navigation and reorganize pages into separate folders

--- frontend/landing-page/src/App.tsx
+++ frontend/landing-page/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Hero from './components/Hero';
-import Product from './components/Product';
-import About from './components/About';
+import { ProductContent } from './pages/product';
+import { AboutContent } from './pages/about';
 
 const App: React.FC = () => {
   return (
@@ -15,8 +15,18 @@ const App: React.FC = () => {
             <Hero />
           </div>
         } />
-        <Route path="/product" element={<Product />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/product" element={
+          <div className="min-h-screen bg-gray-50">
+            <Navigation />
+            <ProductContent />
+          </div>
+        } />
+        <Route path="/about" element={
+          <div className="min-h-screen bg-gray-50">
+            <Navigation />
+            <AboutContent />
+          </div>
+        } />
       </Routes>
     </Router>
   );
