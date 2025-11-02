1d5335fca743df9f1f27ee00f3a2bec2376a90ce
nsnhamucsc <nsnham@ucsc.edu>
2025-11-01 22:08:45 -0700

    Added Learning DB API

--- db_supabase/db_test.py
+++ db_supabase/db_test.py
@@ -1,8 +1,19 @@
 import db_util
+import db_level_user_progress_util
 
 #print(db_util.delete_user_by_username("time_test"))
 #print(type(db_util.get_user_id_by_username("pfp_test2")))
-print(db_util.add_user("test_test", "test56787654@gmail.com", "test1"))
-uid = db_util.get_user_id_by_username("test_test")
-print(db_util.verify_password(uid, "test2"))
-print(db_util.verify_password(uid, "test1"))
+
+#print(db_level_user_progress_util.add_learning_user(1))
+#print(db_level_user_progress_util.add_learning_user(14, 1, 1))
+
+"""
+print(db_level_user_progress_util.increment_user_level(14))
+print(db_level_user_progress_util.increment_user_lesson(14))
+print(db_level_user_progress_util.increment_user_level(1))
+print(db_level_user_progress_util.increment_user_lesson(1))
+print(db_level_user_progress_util.reset_user_level(14))
+"""
+print(db_level_user_progress_util.increment_user_lesson_and_reset_user_level(14))
+print("\n--delim--\n")
+print(db_level_user_progress_util.increment_user_lesson_and_reset_user_level(1))
